43f4368968b91b60ded9776675eff735b3e66f09
Elifázio Bernardes <elifazio@gmail.com>
2018-10-17 11:06:30 -0300

    Commit inicial do plugin de tema do cotuba

---
+++ src/main/java/br/com/paradizo/tema/TemaParadizo.java
@@ -0,0 +1,20 @@
+package br.com.paradizo.tema;
+
+import cotuba.domain.Ebook;
+import cotuba.plugin.Plugin;
+
+public class TemaParadizo implements Plugin {
+
+	@Override
+	public String cssDoTema() {
+		return FileUtils.getResourceContents("/tema.css");
+	}
+
+	@Override
+	public void aposGeracao(Ebook ebook) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
